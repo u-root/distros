orangecms/webboot

add run-webboot.sh script to launch QEMU for webboot
Signed-off-by: Daniel Maslowski <info@orangecms.org>

run-webboot.sh
@@ -1,0 +1,18 @@
+#!/bin/sh
+
+# https://docs.pmem.io/persistent-memory/getting-started-guide/creating-development-environments/virtualization/qemu
+# PMEM *must* be less than RAM so the system has some memory to work with
+export PMEM_SIZE=1G
+export RAM_SIZE=4G
+export KERNEL=$1
+export INITRD=$2
+
+qemu-system-x86_64 \
+  -m $RAM_SIZE \
+  -object rng-random,filename=/dev/urandom,id=rng0 \
+  -device virtio-rng-pci,rng=rng0 \
+  -netdev user,id=u1 -device e1000,netdev=u1 \
+  -kernel $KERNEL \
+  -initrd $INITRD \
+  -append "console=ttyS0 vga=786 memmap=$PMEM_SIZE!$PMEM_SIZE" \
+  -serial stdio

u-root/distros

Signed-off-by: Ronald G. Minnich <rminnich@gmail.com>

run-webboot.sh
@@ -1,0 +1,19 @@
+#!/bin/sh
+
+# https://docs.pmem.io/persistent-memory/getting-started-guide/creating-development-environments/virtualization/qemu
+# PMEM *must* be less than RAM so the system has some memory to work with
+export PMEM_SIZE=1G
+export RAM_SIZE=4G
+export KERNEL=$1
+export INITRD=$2
+
+qemu-system-x86_64 \
+  -machine q35 \
+  -m $RAM_SIZE \
+  -object rng-random,filename=/dev/urandom,id=rng0 \
+  -device virtio-rng-pci,rng=rng0 \
+  -netdev user,id=network0 -device rtl8139,netdev=network0 \
+  -kernel $KERNEL \
+  -initrd $INITRD \
+  -append "console=ttyS0 vga=786 memmap=$PMEM_SIZE!$PMEM_SIZE" \
+  -serial stdio